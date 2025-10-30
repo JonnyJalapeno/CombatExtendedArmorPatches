cd072af39c339c5bd0ada9f041553ca4d116e358
jonnyjalapeno <piotrburz@gmail.com>
2025-10-30 22:08:16 +0100

    g

--- Source/Antibiotics/Hediff_Antibiotics.cs
+++ Source/Antibiotics/Hediff_Antibiotics.cs
@@ -3,7 +3,6 @@ using Verse;
 namespace CombatExtendedArmorPatches
 {
     public class Hediff_Antibiotics : HediffWithComps
-    {
-        // no extra code needed if you just want it to disappear naturally
+    { 
     }
 }
